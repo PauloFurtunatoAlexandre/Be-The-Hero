6e1d563c130a9baf293007a05c0f9c5470cebd8f
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-03-25 17:52:55 -0500

    added controller for incident, add, update and delete cases

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const NgoController = require('./controllers/NgoController');
+const IncidentController = require('./controllers/IncidentController');
+
 const routes = express.Router();
 
-routes.post('/ngos', (request, response) => {
-    const data = request.body;
+routes.get('/ngos', NgoController.index);
+routes.post('/ngos', NgoController.create);
 
-    console.log(data);
-
-    return response.json({});
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
